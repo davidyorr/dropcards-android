9deb6a8b74870eea0920e1bb7c717b4733631f9a
davidyorr <davidyorr@gmail.com>
2015-09-03 01:37:30 -0400

    Set LoginActivity to use activiy_login.xml for its layout

--- app/src/main/java/com/mangoshine/mangocards/ui/LoginActivity.java
+++ app/src/main/java/com/mangoshine/mangocards/ui/LoginActivity.java
@@ -2,6 +2,8 @@ package com.mangoshine.mangocards.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
+import butterknife.ButterKnife;
+import com.mangoshine.mangocards.R;
 
 /**
  *
@@ -10,5 +12,8 @@ public class LoginActivity extends Activity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
+    setContentView(R.layout.activity_login);
+    ButterKnife.bind(this);
   }
 }
