cd6f7bf3d9dd8f4149e636f248f99316bef38416
davidyorr <davidyorr@gmail.com>
2015-09-11 00:29:25 -0400

    Add a navigation drawer

--- app/src/main/java/com/mangoshine/mangocards/ui/HomeActivity.java
+++ app/src/main/java/com/mangoshine/mangocards/ui/HomeActivity.java
@@ -2,6 +2,12 @@ package com.mangoshine.mangocards.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import com.mangoshine.mangocards.DropboxManager;
 import com.mangoshine.mangocards.MangocardsApp;
 import com.mangoshine.mangocards.R;
@@ -12,6 +18,9 @@ import javax.inject.Inject;
 
 public class HomeActivity extends Activity implements HomeView {
 
+  @Bind(R.id.toolbar) Toolbar toolbar;
+  @Bind(R.id.main_drawer_layout) DrawerLayout drawerLayout;
+
   @Inject HomePresenter presenter;
   @Inject DropboxManager dropboxManager;
 
@@ -20,5 +29,12 @@ public class HomeActivity extends Activity implements HomeView {
     ((MangocardsApp) getApplication()).component().plus(new HomeModule(this, dropboxManager)).inject(this);
 
     setContentView(R.layout.activity_home);
+    ButterKnife.bind(this);
+
+    toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        drawerLayout.openDrawer(GravityCompat.START);
+      }
+    });
   }
 }
