b9aa8af19c9a6d1baa85e5b416bd978bd8d317b1
davidyorr <davidyorr@gmail.com>
2015-10-21 00:44:42 -0400

    Plant a Timber DebugTree in debug mode

--- app/src/main/java/com/mangoshine/mangocards/MangocardsApp.java
+++ app/src/main/java/com/mangoshine/mangocards/MangocardsApp.java
@@ -1,12 +1,17 @@
 package com.mangoshine.mangocards;
 
 import android.app.Application;
+import timber.log.Timber;
 
 public class MangocardsApp extends Application {
   private MangocardsAppComponent appComponent;
 
   @Override public void onCreate() {
     super.onCreate();
+
+    if (BuildConfig.DEBUG) {
+      Timber.plant(new Timber.DebugTree());
+    }
 
     appComponent = DaggerMangocardsAppComponent.builder()
         .mangocardsModule(new MangocardsModule(this))
